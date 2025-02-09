cadb37b5395a7867d1658a5bf833f1b1efd3eb81
Lukas <luk.kolar@seznam.cz>
2025-02-09 23:10:06 +0100

    Implemented removing row when the blocks fills the width of game field

--- tetris js/src/Components/Game/GamePage.tsx
+++ tetris js/src/Components/Game/GamePage.tsx
@@ -11,22 +11,22 @@ const isInNumberRange = (rangeStart: number, rangeEnd: number, number: number) =
 
 const GamePage: FC<{ onReturnClick: () => void}> = ({ onReturnClick}) => { 
     const [gaming, setGaming] = useState(false);
-    const [blockPosition, setBlockPosition] = useState({ column: 0, row: 0, width: 3, height: 1});
-    const [finishedBlocks, setFinishedBlocks] = useState<{column: number, row: number}[]>([]);
+    const [blockPosition, setBlockPosition] = useState<{movingBlock: { column: number; row: number; width: number; height: number}; finishedBlocks:  { column: number; row: number}[]}>({movingBlock: { column: 0, row: 0, width: Math.ceil((Math.random() * 4)), height: 1}, finishedBlocks: []});
     const [gameOver, setGameOver] = useState(false);
-    const dropSpeedRef = useRef(500);
+    const dropSpeedRef = useRef(200);
     const oneGridBlockProportions = '40px'
-    const gameWidth = 17;
+    const gameWidth: number = 17;
     const gameHeight = 22;
-    
+    const rowToRemove = useRef<number>(null);
+
     const createGameField = () => {
         const playGround: JSX.Element[][] = []
         for(let i = 0; i < gameHeight; i++){
             const row = [];           
             for(let x = 0; x < gameWidth; x++){
-                const isVisibleBlock = blockPosition.column === x && blockPosition.row === i;
-                const isPartOfBlock = blockPosition.width > 1 ? ((isInNumberRange(blockPosition.column, blockPosition.column + blockPosition.width, x) && blockPosition.row === i)) : false
-                const isFinishedBlock = finishedBlocks.find(block => block.column === x && block.row === i);
+                const isVisibleBlock = blockPosition.movingBlock.column === x && blockPosition.movingBlock.row === i;
+                const isPartOfBlock = blockPosition.movingBlock.width > 1 ? ((isInNumberRange(blockPosition.movingBlock.column, blockPosition.movingBlock.column + blockPosition.movingBlock.width, x) && blockPosition.movingBlock.row === i)) : false
+                const isFinishedBlock = blockPosition.finishedBlocks.find(block => block.column === x && block.row === i);
                 const block = <div key={`${x} && ${i}`} style={{ width: oneGridBlockProportions, height: oneGridBlockProportions, border: '1px solid', backgroundColor: isVisibleBlock || isFinishedBlock || isPartOfBlock ? 'black' : 'transparent' }}>{i}-{x}</div>
                 row.push(block);
             }
@@ -38,17 +38,16 @@ const GamePage: FC<{ onReturnClick: () => void}> = ({ onReturnClick}) => {
     const hanldeKeyUp = (e: KeyboardEvent) => {
         if(e.code === 'Space'){
             e.preventDefault();
-            dropSpeedRef.current = 500;
+            dropSpeedRef.current = 100;
         }
     }
 
     const handleArrowKeyDown = (e: KeyboardEvent) => {
-        console.log(e)
         if(e.code === 'ArrowLeft'){
-            setBlockPosition(prev => ({...prev, column: prev.column ? prev.column - 1 : 0}));
+            setBlockPosition(prev => ({...prev, movingBlock: {...prev.movingBlock, column: prev.movingBlock.column ? prev.movingBlock.column - 1 : 0}}));
         }
         if(e.code === 'ArrowRight'){
-            setBlockPosition(prev => ({ ...prev, column: prev.column === gameWidth - 1 ? prev.column : prev.column + 1 }));
+            setBlockPosition(prev => ({ ...prev, movingBlock: {...prev.movingBlock, column: prev.movingBlock.column === gameWidth - 1 ? prev.movingBlock.column : prev.movingBlock.column + 1} }));
         }
         if(e.code === 'Space'){
             e.preventDefault();
@@ -66,24 +65,6 @@ const GamePage: FC<{ onReturnClick: () => void}> = ({ onReturnClick}) => {
         }
     })
 
-    const dropNewBlock = () => {
-        if(blockPosition.row === 0){
-            setGameOver(true);
-            return;
-        }
-        dropSpeedRef.current = 500;
-        setFinishedBlocks(prev => {
-            prev.push({ row: blockPosition.row, column: blockPosition.column});
-            if(blockPosition.width){
-                const isStillBlock = blockPosition.column + blockPosition.width;
-                for(let blockPart = blockPosition.column; blockPart < isStillBlock; blockPart++){
-                    prev.push({ row: blockPosition.row, column: blockPart});
-                }
-            }
-            return prev;
-        });
-    }
-
     useEffect(() => {
         if(!gaming){
             return;
@@ -91,21 +72,38 @@ const GamePage: FC<{ onReturnClick: () => void}> = ({ onReturnClick}) => {
 
         const blockMovingInterval = setInterval(() => {
             setBlockPosition(prev => {    
-                if(prev.row === gameHeight - 1 || finishedBlocks.find(b => b.row === prev.row + 1 && isInNumberRange(prev.column, prev.column + prev.width, b.column))){
-                    dropNewBlock();
-                    return { column: 0, row: 0, height: 1, width: Math.ceil((Math.random() * 4))}
+                if(prev.movingBlock.row === gameHeight - 1 || prev.finishedBlocks.find(b => b.row === prev.movingBlock.row + 1 && isInNumberRange(prev.movingBlock.column, prev.movingBlock.column + prev.movingBlock.width, b.column))){
+                    const finishedBlocks = [...prev.finishedBlocks];
+                    const currentRow = prev.movingBlock.row
+                    const range = prev.movingBlock.column + prev.movingBlock.width;
+                    for(let blockPart = prev.movingBlock.column; blockPart < range; blockPart++){
+                        finishedBlocks.push({ row: blockPosition.movingBlock.row, column: blockPart});
+                    }
+                    const actuallRowBlockNumber = finishedBlocks.filter(b => b.row === prev.movingBlock.row).length;
+
+                    if(actuallRowBlockNumber === gameWidth){
+                        rowToRemove.current = currentRow; 
+                    }
+                    return {finishedBlocks, movingBlock: {column: 0, row: 0, height: 1, width: Math.ceil((Math.random() * 4))}};
                 } else {
-                    return {...prev, row: prev.row + 1};
+                    return {...prev, movingBlock: {...prev.movingBlock, row: prev.movingBlock.row + 1}};
                 }
             });
+            if(rowToRemove.current){
+                const removedRow: number = rowToRemove.current;
+                setBlockPosition(prev => {
+                    const finishedWithRemovedRow = [...prev.finishedBlocks].filter(b => b.row !== removedRow).map(b => b.row < removedRow! ? {...b, row: b.row + 1} : b);
+                    return { ...prev, finishedBlocks: finishedWithRemovedRow};
+                });
+                rowToRemove.current = null;
+            }
         }, dropSpeedRef.current);
 
         return () =>  clearInterval(blockMovingInterval);
     });
 
     const startNewGame = () => {
-        setFinishedBlocks([]);
-        setBlockPosition({ column: 0, row: 0, height: 0, width: Math.ceil((Math.random() * 4))});
+        setBlockPosition({movingBlock: { column: 0, row: 0, height: 0, width: Math.ceil((Math.random() * 4))}, finishedBlocks: []});
         setGameOver(false);
     }
 
